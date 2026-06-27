c7abe3dc83aa5a93a1d08c965cb2aaca9ebf9716
Atharva Nayak <atharvanayak2006@gmail.com>
2026-06-28 01:29:48 +0530

    Solved reverse a linked list problem

---
+++ src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,52 @@
+package LinkedList;
+import java.util.Stack;
+public class ReverseLinkedList {
+    static ListNode head;
+    static ListNode tail;
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int data) {
+            this.val = data;
+        }
+
+        public ListNode(int data, ListNode next) {
+            this.val = data;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        ListNode temp = head;
+        while (temp != null) {
+            stack.push(temp.val);
+            temp = temp.next;
+        }
+        temp = head;
+
+        while (temp != null) {
+            temp.val = stack.pop();
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        // Creating linked list 1 -> 2 -> 3 -> null
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+
+        ReverseLinkedList sol = new ReverseLinkedList();
+        head = sol.reverseList(head);
+
+        // Printing reversed list
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
